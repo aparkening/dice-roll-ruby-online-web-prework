49337672819ccd8fbf2e99e9a4ffb50484baf8d4
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-18 16:55:06 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,7 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
- this_roll = rand(1...7)
- puts "This role is ${this_roll}"
- return this_roll
+ rand(1...7)
 end
+
+ puts "This role is ${this_roll}"
